7266682eafecd7b6cbb9b1e9da1e2157ba6e0550
Trémor Lydia <84137424+Lydia14777@users.noreply.github.com>
2023-08-21 16:45:34 +0200

    étape 4

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+//     innerHTML: Modifiez le slide au clic sur le bouton
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -17,21 +18,49 @@ const slides = [
 	}
 ]
 
+function slider () {
+	//for (let i = 0; i <= 0; i++)
+	for (let i = 0; i <= 4; i++) {
+		let hero = document.getElementById("banner").innerHTML =
+		//Récupère l'image = image de la flèche gauche
+		document.querySelector(".arrow_left")
+		//Récupère l'image = image slide
+		const imageElement = document.querySelector(".banner-img")
+		//Récupère le texte = tagLine
+		document.getElementsbyTagName('p').innerHTML = "tagLine"
+		//Récupère l'image = image de la flèche droite
+		document.querySelector(".arrow_right")
+		//On récupère les bullets points
+		document.querySelector(".dots")
+	}
+}
+slider();
+
+//     const carousel_arrows = document.addEventListener('click', carousel_arrows);
+
+// Récupération des flèches dans le DOM
 let fleche_gauche = document.querySelector(".arrow_left");
 console.log(fleche_gauche);
-fleche_gauche.addEventListener("click", function() {
-	console.log("Ca fonction");
-});
 
 let fleche_droite = document.querySelector(".arrow_right");
 console.log(fleche_droite);
-fleche_droite.addEventListener("click", function() {
-	console.log("Ca fonction");
-});
 
-/*const arrows = {
-	"image":"arrow_left",
-	"image":"arrow_left"
-}*/
+//Création d'un évènement au click
+function event_arrows {
+	fleche_gauche.addEventListener("click", function() {
+		console.log("Ca fonctionne");
+	});
 	
-/*const carousel_arrows = document.addEventListener('click', carousel_arrows);*/
+	fleche_droite.addEventListener("click", function() {
+		console.log("Ca fonction");
+	});
+}
+event_arrows();
+
+
+
+
+
+
+	
+
