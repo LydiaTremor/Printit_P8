a57026bfee0de45817465b02e9e83d5c96cf932b
Trémor Lydia <84137424+Lydia14777@users.noreply.github.com>
2023-10-23 09:35:23 +0200

    soutenance

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-//////////////  TABLEAU contenant une image, un attribut alt et un tagLine    
+// Tableau
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -22,24 +22,20 @@ const slides = [
 	}
 ];
 
-//Récupération des flèches dans le DOM
+// Récupération des flèches dans le DOM
 let fleche_gauche = document.querySelector(".arrow_left");
 let fleche_droite = document.querySelector(".arrow_right");
 let currentIndex = 0;
 
-
+// Une boucle pour le slide
 function slider() {
 
     slides.forEach((element, index) => {
         const dot = document.createElement('div');
 
-		//Condition avec innerHTML: Modifiez le slide au clic sur le bouton
-		//La méthode parseInt() : convertit une chaine en un nombre entier 
-		//Si le 1er élément de l'index est triplement également (égalité stricte en valeur et en type) à 0
         if(parseInt(index) === 0){  
             dot.classList.add('dot_selected');
 
-            //Afficher la première diapositive
             document.querySelector('.banner-img').src = './assets/images/slideshow/'+element.image;
 			document.querySelector('.banner-img').alt = element.alt;
 			document.querySelector('#banner > p').innerHTML = element.tagLine;
@@ -54,8 +50,9 @@ function slider() {
 
 slider();
 
-//////////////     UNE BOUCLE POUR LE SLIDE
+// Une boucle pour les bullets points du slide
 function bullet_points(dotIndex) {
+	
 	Array.from(document.querySelectorAll('.dot')).forEach((element, index) => {
 		if(parseInt(dotIndex) === index) {
 			element.classList.add('dot_selected');
@@ -69,16 +66,10 @@ function bullet_points(dotIndex) {
 function showSlide(direction) {
     currentIndex = currentIndex + direction;
 	
-	//Condition pour afficher le slide infini
 	if(currentIndex >= slides.length) {currentIndex = 0;}else if(currentIndex <= 0)  {currentIndex = slides.length - 1;}
 
-	//*** Le texte change en accord avec l’image montrée
     document.querySelector('.banner-img').src = './assets/images/slideshow/'+slides[currentIndex].image;
-
-	//On récupère l'attribut alt en ciblant la class banner-img
     document.querySelector('.banner-img').alt = slides[currentIndex].alt;
-
-	//On récupère texte en ciblant tous les p de l'id banner + modification du texte avec innerHTML en récupérant les tagLine
     document.querySelector('#banner > p').innerHTML = slides[currentIndex].tagLine;
 
 	bullet_points(currentIndex);
